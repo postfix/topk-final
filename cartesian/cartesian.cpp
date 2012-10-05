468b402e4b5c886592381c2b210e06f5ef7da8ab
Roberto Konow <rkonowkr@gmail.com>
2012-10-05 16:19:00 -0400

    fixed rmq (fclaude)

--- cartesian/cartesian.cpp
+++ cartesian/cartesian.cpp
@@ -1,84 +1,220 @@
-
 #include <iostream>
 #include <utility>
 #include <stdlib.h>
+#include <vector>
+#include <tree_ff.h>
+#include <BitString.h>
+
 using namespace std;
-class CartesianNode {
-public:
-    CartesianNode *left;
-    CartesianNode *right;
-    size_t array_pos;
-    size_t array_value;
+namespace rmq_cartesian {
+    class CartesianNode {
+    public:
+        CartesianNode *left;
+        CartesianNode *right;
+        size_t array_pos;
+        size_t array_value;
 
-    CartesianNode(size_t a,size_t b) {
-        this->array_pos = a;
-        this->array_value = b;
-    }    
-};
-class CartesianTree {
-public: 
-    size_t *array;
-    size_t n;
-    CartesianNode *root;
+        CartesianNode(size_t a,size_t b) {
+            this->array_pos = a;
+            this->array_value = b;
+            this->left = NULL;
+            this->right = NULL;
+        }
+        CartesianNode() {
+            this->left = NULL;
+            this->right = NULL;
+            this->array_pos = (size_t)-1;
+        };    
+    };
+    class CartesianQueue {
+    public:
+        CartesianNode *n;
+        size_t ini;
+        size_t fin; 
+        CartesianQueue(CartesianNode *n,size_t ini, size_t fin) {
+            this->ini = ini;
+            this->fin = fin;
+            this->n = n;
+        }
+    };
+
+    class CartesianTree {
+    public: 
+        size_t *array;
+        size_t n;
+        CartesianNode *root;
+        tree_ff *t;
 
 
-    pair<size_t,size_t> findMax(size_t ini,size_t fin) {
-        size_t max = this->array[ini];
-        size_t max_pos = ini;
-        for (size_t i = ini + 1; i < fin;i++) {
-            if (max < this->array[i]) {
-                max = this->array[i];
-                max_pos = i;
+            pair<size_t,size_t> findMax(size_t ini,size_t fin) {
+                size_t max = this->array[ini];
+                size_t max_pos = ini;
+                for (size_t i = ini + 1; i < fin;i++) {
+                    if (max < this->array[i]) {
+                        max = this->array[i];
+                        max_pos = i;
+                    }
+                }
+                return make_pair(max_pos,max);
+            }
+            void buildTree(vector<pair<CartesianNode*,pair<size_t,size_t> > > *q) {
+                while(!q->empty()) {        
+                    pair<CartesianNode*,pair<size_t,size_t> > r = q->back();
+                    CartesianNode *node = r.first;
+                    q->erase(q->end());
+                    size_t ini = r.second.first;
+                    size_t fin = r.second.second;
+            	    if (fin == (size_t)-1) continue;         
+            	    if (ini == fin) continue;
+                    
+            	    cout << "entered with:" << ini << "," << fin << endl;
+                    pair<size_t,size_t> max = findMax(ini,fin);
+            	    cout << "max is = " << max.second << endl;
+            	    cout << "pos is = " << max.first << endl;
+                    if (node->array_pos == (size_t)-1) {
+                        node->array_pos = max.first;
+                        node->array_value = max.second;
+                        node->left = new CartesianNode();
+                        node->right = new CartesianNode();              
+
+                        pair<CartesianNode*,pair<size_t,size_t> > aux_right = make_pair(node->right,make_pair(max.first+1,fin));
+                        q->push_back(aux_right);
+                        pair<CartesianNode*,pair<size_t,size_t> > aux_left = make_pair(node->left,make_pair(ini,max.first));
+                        q->push_back(aux_left);
+                    }
             }
         }
-        return make_pair(max_pos,max);
-    }
-    void buildTree(CartesianNode*& n, size_t ini,size_t fin) {
-        if (n == NULL) {
-	    if (fin == (size_t)-1) return;         
-	    if (ini == fin) {
-            //	n = new CartesianNode(ini,this->array[ini]);
-		return;
-	    }
 
-//	    cout << "entered with:" << ini << "," << fin << endl;
-            pair<size_t,size_t> max = findMax(ini,fin);
-//	    cout << "max is = " << max.second << endl;
-//	    cout << "pos is = " << max.first << endl;
-            n = new CartesianNode(max.first,max.second);
-            n->left = NULL;
-            n->right = NULL;
-	            
-            buildTree(n->left,ini,max.first);
-            buildTree(n->right,max.first+1,fin);
+
+            void preorder() {
+                vector<CartesianNode*> q;
+                q.push_back(this->root);
+                while (!q.empty()) {
+                    CartesianNode *n = q.back();
+                    q.pop_back();
+                    if (n->array_pos == (size_t)-1) continue;
+                    cout << n->array_pos << endl;
+                    q.push_back(n->right);
+                    q.push_back(n->left);
+                }
+            }
+
+            void preorder_normal(CartesianNode *n) {
+                if (n->array_pos == (size_t)-1 ) {
+                    // cout << "()";
+                    return;
+                }
+                //cout << n->array_pos << endl;
+                
+                cout << "(" ; 
+                preorder_normal(n->left);
+                preorder_normal(n->right);
+                cout << ")";
+            }
+
+            tree_ff *buildCompactTree() {
+                vector<pair<bool,CartesianNode*> > q;
+                q.push_back(make_pair(false,this->root));
+                cds_utils::BitString *bs = new  cds_utils::BitString(2*(this->n));
+                uint pos = 0;
+                while (!q.empty()) {
+                    CartesianNode *n = q.back().second;
+                    bool closing = q.back().first;
+                    q.pop_back();
+                    if (closing) {
+                        bs->setBit(pos);
+                        cout << ")";
+                        pos++;      
+                    } else {
+                        pos++;
+                        cout << "(";
+                        q.push_back(make_pair(true, n));
+                        if (n->right->array_pos != (size_t)-1)
+                          q.push_back(make_pair(false, n->right));
+                        if (n->left->array_pos != (size_t)-1)
+                           q.push_back(make_pair(false, n->left));    
+                    }
+                    
+                }
+     
+                cout << "tree->" << endl;     
+                for (int i = 0 ; i < 2*(this->n) ; i++ ) {
+                    if (bs->getBit(i) == 0 ) cout << "(";
+                    else cout << ")";
+                }  
+                cout << endl;
+                this->t = new tree_ff(bs->getData(),2*(n),OPT_FAST_LCA | OPT_FAST_PREORDER_SELECT | OPT_FAST_LEAF_SELECT);
+            }
+            CartesianTree(size_t *array,size_t n) {
+                this->array = array;
+                this->root = new CartesianNode();
+            	this->n = n;
+                vector<pair<CartesianNode*,pair<size_t,size_t> > >  *ranges = new vector<pair<CartesianNode*,pair<size_t,size_t> > > ();
+                ranges->push_back(make_pair(this->root,make_pair(0,n)));
+                buildTree(ranges);
+                preorder();
+                cout << "---------" << endl;
+                preorder_normal(this->root);
+                cout << "end tree" << endl;
+                buildCompactTree();
+
+            }
+
+            uint query(uint i, uint j) {
+                uint v1 = this->t->Preorden_Select(i+1);
+                uint v2 = this->t->Preorden_Select(j+1);
+                uint v = this->t->Lca(v1,v2);   
+                if (v==v1) return i;
+                uint d1 = this->t->Depth(v);
+                uint d2 = this->t->Depth(v2);
+                uint vv = this->t->Level_Ancestor(v2,-(d2-d1)+1);
+                return this->t->Preorder_Rank(vv-1)-1;
+            }
+
+            bool testRMQ(int n) { 
+            size_t *A = new size_t[n];
+            size_t *B = new size_t[n];
+            for (int i = 0 ; i < n;i++)
+            {
+                //cout << "rand = " << rand()%100;
+                A[i] = rand()%100;
+                B[i] = A[i];
+            }
+            
+            int i;
+            //RMQ *rmq = new RMQ(B,n);
+            CartesianTree * ct = new CartesianTree(B,n);
+            int min = 0;
+            for (int i = 0 ; i <n;i++){
+                min = i;
+                for (int j = i+1;j<n;j++) {
+                    if (A[min] >= A[j])
+                        min = j;
+                    int rmq_min = ct->query(i,j);
+                    if (rmq_min != min) {
+                        cout << "(" << i << " , " << j << " ) wrong -> " << rmq_min << " vs " << min << endl;
+                    //  return false;
+                    } else {
+                        cout << "(" << i << " , " << j << " ) RIGHT -> " << rmq_min << " vs " << min << endl;
+                    }
+                }
+                
+            }
+            cout << "RMQ test completed..." << endl;
+          //  cout << "RMQ size = " << rmq->getSize() << endl;
+            return true;
         }
-    }
-    void preorder(CartesianNode *&n) {
-   	 if (n == NULL) return;
-   	 cout << n->array_value;
-         cout << "(";
-	 preorder(n->left);
-	 cout << ")";
-	 cout << "(";
-         preorder(n->right);
-	 cout << ")";
-    }
-    CartesianTree(size_t *array,size_t n) {
-        this->array = array;
-        this->root = NULL;
-	this->n = n;
-        buildTree(root,0,n);
-	cout << "(";
-        preorder(root);
-	cout << ")";
-    }
+
+    };
 };
 
+using namespace rmq_cartesian;
 int main() {
-	size_t n = 10000000;
+	size_t n = 10;
 	size_t *a = new size_t[n];
 	for (size_t i = 0 ; i < n;i++) {
-		a[i] = rand()%3;
+		a[i] = rand()%10;
 	}
 	CartesianTree * ct = new CartesianTree(a,n);
+    ct->testRMQ(10);
 }
--- src/rmq.cpp
+++ src/rmq.cpp
@@ -1,4 +1,9 @@
+
+#include <exception>
+#include <vector>
+
 #include <rmq.h>
+
 
 using namespace rmq_space;
 RMQ::RMQ(int *A,size_t n)
@@ -38,60 +43,119 @@ size_t RMQ::getSize()
 
 int RMQ::mapTree (unsigned int *bitmap, Tree *T, int i)
 {
-	  int W2 = 32;
+	int W2 = 32;
 //	  if (i >= n) cout << "WTF! i = " << i  << " n = " << n << endl;
-	  uint total = (2*(n+1)+W2-1)/W2; 
-	  if(i/W2 < total) {
-	  if (T == NULL) return i;
-	   i = mapTree (bitmap, T->prevSibl, i);
-
-	   if (i/W2 < total)  {
-		   bitmap[i/W2] &= ~(1<<(i%W2)); i++; // parentesis[i++] = 0;
-	   }  else  {
-		return total;
-	   }
-
-	   i = mapTree (bitmap, T->lastChild, i);
-
-	   if (i/W2 < total)  {
-		   bitmap[i/W2] |= 1<<(i%W2); i++; // parentesis[i++] = 1;
-	   } else { 
-		return total;
-            }
-	   } else {
-	   return total;
-	   }
+	uint total = (2*(n+1)+W2-1)/W2; 
+	  
+	vector<pair<int, Tree*> > stack;
+	stack.push_back(make_pair(1, T));
+	while(!stack.empty()) {
+		int first = stack.back().first;
+		Tree *second = stack.back().second;
+		stack.pop_back();
+	   	if (second == NULL) throw "WTF!";
+	   	if (first == 0) {
+	   		//i = mapTree (bitmap, T->prevSibl, i);
+	   		// stack.push_back(make_pair(1, second));
+	   		Tree *T_tmp = second;
+	   		while (T_tmp != NULL) {
+	   			stack.push_back(make_pair(1, T_tmp));
+	   			T_tmp = T_tmp->prevSibl;
+	   		}
+	   	} else if (first == 1) {
+	   		bitmap[i/W2] &= ~(1<<(i%W2)); i++; // parentesis[i++] = 0;
+	   		stack.push_back(make_pair(2, second));
+	   		if (second->lastChild)
+	   			stack.push_back(make_pair(0, second->lastChild));
+	   	} else if (first == 2) {
+	   		bitmap[i/W2] |= 1<<(i%W2); i++; // parentesis[i++] = 1;
+	   	} else {
+	   		cout << "i : " << i << endl;
+	   		cout << "Something went really wrong!" << endl;
+	   		throw "wtf";
+	   	}
+	}
 }
 
 
+// unsigned int *RMQ::buildrmq (int *A, size_t n)
+// {
+// 	  int W2 = 32;
+// 	  Tree *T = new Tree();
+// 	  Path *P = new Path(); // lista bottom up del rightmost path
+// 	  size_t i;
+// 	  unsigned int *bitmap;
+	 
+// 	  //T = (Tree)malloc (sizeof(struct sTree));
+// 	  T->prevSibl = T->lastChild = NULL;
+// 	  T->pos = -1; // fake root
+// 	  //P = (Path)malloc (sizeof(struct sPath));
+// 	  P->node = T;
+// 	  P->next = NULL;
+
+// 	  for (i=0;i<n;i++) // ubicar A[i] en el rightmost path
+// 	  {
+// 	          Path *p;
+// 		  Tree *t;
+// 		  while ((P->node->pos != -1) && (A[P->node->pos] >= A[i]))
+// 	    	   {
+// 	       		 p = P->next;
+// 	//		 delete P;
+// 		         P = p;
+// 	    	   }
+// 			// found the insertion point
+// 	//	  cout << "pos = " << i << endl;
+// 		  t = new Tree(); //(Tree)malloc(sizeof(struct sTree));
+// 		  t->pos = i;
+// 		  t->lastChild = NULL;
+// 		  t->prevSibl = P->node->lastChild;
+// 		  P->node->lastChild = t;
+// 	  //  printf ("A[%i]=%i se cuelga de A[%i]=%i\n",i,A[i],P->node->pos,A[P->node->pos]);
+// 		  p = new Path(); //(Path)malloc(sizeof(struct sPath));
+// 		  p->node = t;
+// 		  p->next = P;
+// 		  P = p;
+// 	  }
+// 	// finished T; free P and convert T into parentheses
+// 	  while (P != NULL)
+// 	  {
+// 	    Path *p = P->next;
+// 	  //  delete P;//free(P);
+// 	    P = p;
+// 	  }
+// 	  bitmap = (unsigned int*)malloc (sizeof(unsigned int) * ((2*(n+1)+W2-1)/W2));
+// 	  mapTree (bitmap,T,0);
+// //	  delete T;
+// 	  // delete t;
+// 	  return bitmap;
+// }
+
 unsigned int *RMQ::buildrmq (int *A, size_t n)
 {
-	  int W2 = 32;
 	  Tree *T = new Tree();
 	  Path *P = new Path(); // lista bottom up del rightmost path
-	  size_t i;
+	  int i;
 	  unsigned int *bitmap;
 	 
 	  //T = (Tree)malloc (sizeof(struct sTree));
 	  T->prevSibl = T->lastChild = NULL;
 	  T->pos = -1; // fake root
-	  cout << "T->Pos = " << T->pos << endl; 
+	  
 	  //P = (Path)malloc (sizeof(struct sPath));
 	  P->node = T;
 	  P->next = NULL;
 
 	  for (i=0;i<n;i++) // ubicar A[i] en el rightmost path
 	  {
-	          Path *p;
+	      Path *p;
 		  Tree *t;
 		  while ((P->node->pos != -1) && (A[P->node->pos] >= A[i]))
-	    	   {
-	       		 p = P->next;
-	//		 delete P;
-		         P = p;
-	    	   }
+	    {
+	        p = P->next;
+		      delete P;
+		      P = p;
+	    }
 			// found the insertion point
-	//	  cout << "pos = " << i << endl;
 		  t = new Tree(); //(Tree)malloc(sizeof(struct sTree));
 		  t->pos = i;
 		  t->lastChild = NULL;
@@ -107,16 +171,15 @@ unsigned int *RMQ::buildrmq (int *A, size_t n)
 	  while (P != NULL)
 	  {
 	    Path *p = P->next;
-	  //  delete P;//free(P);
+	    delete P;//free(P);
 	    P = p;
 	  }
-	  bitmap = (unsigned int*)malloc (sizeof(unsigned int) * ((2*(n+1)+W2-1)/W2));
+	  bitmap = (unsigned int*)malloc (sizeof(unsigned int) * ((2*(n+1)+W-1)/W));
 	  mapTree (bitmap,T,0);
-//	  delete T;
+	  delete T;
 	  // delete t;
 	  return bitmap;
 }
-/*
 bool testRMQ(int n)
 {
 	int *A = new int[n];
@@ -150,4 +213,8 @@ bool testRMQ(int n)
 	cout << "RMQ test completed..." << endl;
 	cout << "RMQ size = " << rmq->getSize() << endl;
 	return true;
-}*/
+}
+
+// int main() {
+// 	testRMQ(10000);
+// }
